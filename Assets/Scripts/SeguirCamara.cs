7f12c8a432fd2fc91cb9d21ac1bc66f13091faad
Johan_David <jhoanpor78@gmail.com>
2022-02-23 22:47:55 -0500

    Parcial 2 - DV

--- Assets/Scripts/SeguirCamara.cs
+++ Assets/Scripts/SeguirCamara.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SeguirCamara : MonoBehaviour
 {
     public GameObject player;
+    public GameObject player2;
+    public GameObject player3;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +16,37 @@ public class SeguirCamara : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Vector3 direccion = new Vector3(0f, player.Transform.y, 0f);
+
+        Vector3 direccion = player.transform.position;;
+
+        if( direccion.y <= -15.94 ){
+
+            direccion = player2.transform.position;
+            
+            if( direccion.x <= -16 ){
+                direccion = player3.transform.position;
+                if( direccion.x <= -24 ){
+                    direccion.x = -24f;
+                    direccion.z = -10f;
+                }
+                else{
+                    direccion.y = -16f;
+                    direccion.z = -10f;
+                }
+            }
+            else{
+                direccion.y = -16f;
+                direccion.z = -10f;
+            }
+
+        }
+        else{
+            direccion.z = -10f;
+            direccion.x = 0.5f;
+        }        
+
+        transform.position = direccion;
+
         //Transform.position = direccion;
         //new Vector3(player.transform.position.x, player.transform.position.x, player.transform.position.x);
     }
