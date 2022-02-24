cb80a7cfd9896b73ca7e3a4672875c5667026059
Johan_David <jhoanpor78@gmail.com>
2022-02-23 21:55:04 -0500

    Parcial 2 - DV

---
+++ Assets/Scripts/SeguirCamara.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeguirCamara : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 direccion = new Vector3(0f, player.Transform.y, 0f);
+        //Transform.position = direccion;
+        //new Vector3(player.transform.position.x, player.transform.position.x, player.transform.position.x);
+    }
+}
